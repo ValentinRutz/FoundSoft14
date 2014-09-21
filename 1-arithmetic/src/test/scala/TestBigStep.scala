89c9776f1809abd76b317e7f4633dccb2a086162
Damien Engels <engelsdamien@gmail.com>
2014-09-21 22:54:52 +0200

    Adds tests from course statement for 'big step' evaluator

---
+++ 1-arithmetic/src/test/scala/TestBigStep.scala
@@ -0,0 +1,21 @@
+import fos.Arithmetic._
+import fos._
+import org.scalatest.{ Matchers, FunSuite }
+
+/**
+  * Created by Damien on 21/09/14.
+  */
+class TestBigStep extends FunSuite with Matchers {
+    test("Example 1 from lab statement") {
+        val input = If(IsZero(Pred(Pred(Succ(Succ(Zero))))), If(IsZero(Zero), True, False), False)
+        val output = True
+
+        eval(input) should be(output)
+    }
+
+    test("Example 2 from lab statement") {
+        val input = Pred(Succ(Succ(Succ(False))))
+        val output = Succ(False)
+        eval(input) should be(output)
+    }
+}
