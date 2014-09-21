bac45c8fab0e56772443f652713a7ac4d714e4b2
Damien Engels <engelsdamien@gmail.com>
2014-09-21 21:50:44 +0200

    Adds missing step in example 1, and transforms test to use pretty matcher syntax

--- 1-arithmetic/src/test/scala/TestOneStep.scala
+++ 1-arithmetic/src/test/scala/TestOneStep.scala
@@ -1,28 +1,28 @@
 import fos.Arithmetic._
 import fos._
-import org.scalatest.FunSuite
+import org.scalatest.{ Matchers, FunSuite }
 
 /**
   * Created by Valentin on 18/09/14.
   */
-class TestOneStep extends FunSuite {
+class TestOneStep extends FunSuite with Matchers {
     test("Example 1 from lab statement") {
         val steps: Seq[Term] = Seq(
             If(IsZero(Pred(Pred(Succ(Succ(Zero))))), If(IsZero(Zero), True, False), False),
             If(IsZero(Pred(Succ(Zero))), If(IsZero(Zero), True, False), False),
             If(IsZero(Zero), If(IsZero(Zero), True, False), False),
             If(True, If(IsZero(Zero), True, False), False),
+            If(IsZero(Zero), True, False),
             If(True, True, False),
             True)
 
         for (Seq(input, output) <- steps.sliding(2)) {
-            assert(reduce(input) == output)
+            reduce(input) should be(output)
         }
     }
 
     test("Example 2 from lab statement") {
         val input = Pred(Succ(Succ(Succ(False))))
-        val output = Pred(Succ(Succ(Succ(False))))
-        assert(reduce(input) == output)
+        reduce(input) should be(input)
     }
 }
