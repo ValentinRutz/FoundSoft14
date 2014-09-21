7d546af9b6e599fd5bb508fbc4fb63c99e2d3d2f
Damien Engels <engelsdamien@gmail.com>
2014-09-21 21:42:40 +0200

    Extends one step test to include all the steps from the example given in the lab statement

--- 1-arithmetic/src/test/scala/TestOneStep.scala
+++ 1-arithmetic/src/test/scala/TestOneStep.scala
@@ -6,15 +6,23 @@ import org.scalatest.FunSuite
   * Created by Valentin on 18/09/14.
   */
 class TestOneStep extends FunSuite {
-    test("First example from statement") {
-        val input = If(IsZero(Pred(Pred(Succ(Succ(Zero))))), If(IsZero(Zero), True, False), False)
-        val output = If(IsZero(Pred(Succ(Zero))), If(IsZero(Zero), True, False), False)
-        assert(oneStepEvaluator(input) == output)
+    test("Example 1 from lab statement") {
+        val steps: Seq[Term] = Seq(
+            If(IsZero(Pred(Pred(Succ(Succ(Zero))))), If(IsZero(Zero), True, False), False),
+            If(IsZero(Pred(Succ(Zero))), If(IsZero(Zero), True, False), False),
+            If(IsZero(Zero), If(IsZero(Zero), True, False), False),
+            If(True, If(IsZero(Zero), True, False), False),
+            If(True, True, False),
+            True)
+
+        for (Seq(input, output) <- steps.sliding(2)) {
+            assert(reduce(input) == output)
+        }
     }
 
-    test("Bad example with One Step") {
+    test("Example 2 from lab statement") {
         val input = Pred(Succ(Succ(Succ(False))))
         val output = Pred(Succ(Succ(Succ(False))))
-        assert(oneStepEvaluator(input) == output)
+        assert(reduce(input) == output)
     }
 }
