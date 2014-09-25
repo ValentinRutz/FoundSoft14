2e3da0bc9deca0555af648dab28c89f54bc3cd97
Damien Engels <engelsdamien@gmail.com>
2014-09-25 13:58:36 +0200

    Adds more tests for one step evaluator

--- 1-arithmetic/src/test/scala/TestOneStep.scala
+++ 1-arithmetic/src/test/scala/TestOneStep.scala
@@ -25,4 +25,32 @@ class TestOneStep extends FunSuite with Matchers {
         val input = Pred(Succ(Succ(Succ(False))))
         reduce(input) should be(input)
     }
+
+    test("Regression for IsZero(Succ(Pred(Zero)))") {
+        reduce(IsZero(Succ(Pred(Zero)))) should be(IsZero(Succ(Zero)))
+    }
+
+    test("Simple tests") {
+        val computations = Map(
+            If(True, True, False) -> True,
+            If(False, True, False) -> False,
+            IsZero(Zero) -> True,
+            IsZero(Succ(Zero)) -> False,
+            Pred(Zero) -> Zero,
+            Pred(Succ(Zero)) -> Zero)
+
+        for ((input, output) <- computations) {
+            reduce(input) should be(output)
+        }
+
+        val congruences = Map(
+            If(IsZero(Zero), True, False) -> If(True, True, False),
+            IsZero(Pred(Succ(Zero))) -> IsZero(Zero),
+            Pred(Pred(Zero)) -> Pred(Zero),
+            Succ(Pred(Zero)) -> Succ(Zero))
+
+        for ((input, output) <- congruences) {
+            reduce(input) should be(output)
+        }
+    }
 }
