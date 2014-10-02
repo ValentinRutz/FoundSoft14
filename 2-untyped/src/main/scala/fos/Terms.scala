ed3efbdbdc99b2bd3910e9610c99852cf9b63b13
Valerian Pittet <valerian.pittet@epfl.ch>
2014-10-02 09:16:21 +0200

    compilation compete, sources formatted

--- 2-untyped/src/main/scala/fos/Terms.scala
+++ 2-untyped/src/main/scala/fos/Terms.scala
@@ -5,16 +5,16 @@ import scala.util.parsing.input.Positional
 /** Abstract Syntax Trees for terms. */
 abstract class Term extends Positional
 
-  //   ... To complete ... 
+//   ... To complete ... 
 
 case class Variable(name: String) extends Term {
-  override def toString: String = name
+    override def toString: String = name
 }
 
 case class Abstraction(param: Variable, body: Term) extends Term {
-  override def toString: String = "\\" + Variable.toString + ". (" + body.toString + ")"
+    override def toString: String = "\\" + Variable.toString + ". (" + body.toString + ")"
 }
 
 case class Application(function: Term, argument: Term) extends Term {
-  override def toString: String = "(" + function.toString + ") (" + argument.toString + ")"
+    override def toString: String = "(" + function.toString + ") (" + argument.toString + ")"
 }
