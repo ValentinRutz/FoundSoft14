a32abc3fbe6636e0a355c350b39c6e5a95d0a974
Valerian Pittet <valerian.pittet@epfl.ch>
2014-10-02 15:35:40 +0200

    completing Parser Test

--- 2-untyped/src/test/scala/TestParser.scala
+++ 2-untyped/src/test/scala/TestParser.scala
@@ -1,13 +1,43 @@
 import fos.Untyped._
 import fos._
-import org.scalatest.FunSuite
+import org.scalatest.{ Matchers, FunSuite }
 
-class TestParser extends FunSuite {
-    test("Parser match sample imput") {
+class TestParser extends FunSuite with Matchers {
 
-        val tokens = new lexical.Scanner("x")
-        phrase(Term)(tokens) match {
-            case Success(trees, _) => println(trees)
+    def tokens(input: String) = new lexical.Scanner(input)
+
+    implicit class testingString(input: String) {
+        def =?(expectedTree: Term): Unit = {
+            phrase(Term)(tokens(input)) match {
+                case Success(result, _) => {
+                    result should be(expectedTree)
+                }
+                case _ => fail
+            }
         }
+    }
+
+    test("Test Parsing single var") {
+        val input = "x"
+        val expectedTree = Variable("x")
+        input =? expectedTree
+    }
+
+    test("Test parsing simple abstraction") {
+        val input = "\\x. y"
+        val expectedTree = Abstraction(Variable("x"), Variable("y"))
+        input =? expectedTree
+    }
+
+    test("Test parsing simple application") {
+        val input = "x x"
+        val expectedTree = Application(Variable("x"), Variable("x"))
+        input =? expectedTree
+    }
+
+    test("Test parsing parenthesis variable") {
+        val input = "(x)"
+        val expectedTree = Variable("x")
+        input =? expectedTree
     }
 }
