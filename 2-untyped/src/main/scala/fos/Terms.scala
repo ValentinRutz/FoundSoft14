e3341060dc204e9ded5a770a5c122255c2d642fd
Valerian Pittet <valerian.pittet@epfl.ch>
2014-10-02 08:34:27 +0200

    created prototype terms to write parser

--- 2-untyped/src/main/scala/fos/Terms.scala
+++ 2-untyped/src/main/scala/fos/Terms.scala
@@ -6,3 +6,15 @@ import scala.util.parsing.input.Positional
 abstract class Term extends Positional
 
   //   ... To complete ... 
+
+case class Variable(name: String) extends Term {
+  override def toString: String = name
+}
+
+case class Abstraction(param: Variable, body: Term) extends Term {
+  override def toString: String = "\\" + Variable.toString + ". (" + body.toString + ")"
+}
+
+case class Application(function: Term, argument: Term) extends Term {
+  override def toString: String = "(" + function.toString + ") (" + argument.toString + ")"
+}
