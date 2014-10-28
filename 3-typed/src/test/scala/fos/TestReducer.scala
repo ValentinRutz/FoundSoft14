16bc07df0a60e5c055d487d121bf574057a16cd5
Valerian Pittet <valerian.pittet@epfl.ch>
2014-10-29 00:44:16 +0100

    completed test cases in TestReducer

--- 3-typed/src/test/scala/fos/TestReducer.scala
+++ 3-typed/src/test/scala/fos/TestReducer.scala
@@ -41,4 +41,36 @@ class TestReducer extends FunSuite with Matchers with LambdaTest {
     test("snd test") {
         "snd {0, tru}" shouldReduceTo "tru"
     }
+
+    // CONGRUENCE
+
+    test("if term") {
+        "if iszero 0 then true else false" shouldReduceTo "if true then true else false"
+        "if pred 0 then true else { 0, true }" shouldReduceTo "if 0 then true else { 0, true }"
+    }
+
+    test("iszero term") {
+        "iszero pred 0" shouldReduceTo "iszero 0"
+        "iszero snd {0, true}" shouldReduceTo "iszero true"
+    }
+
+    test("succ term") {
+        "succ pred 0" shouldReduceTo "succ 0"
+        "succ snd {0, true}" shouldReduceTo "succ true"
+    }
+
+    test("pred term") {
+        "pred pred 0" shouldReduceTo "pred 0"
+        "pred snd {0, true}" shouldReduceTo "pred true"
+    }
+
+    test("value term") {
+        "(\\x: Nat. pred 0) pred 0" shouldReduceTo "(\\x: Nat. pred 0) 0"
+        "(\\x: Bool. pred true) pred 0" shouldReduceTo "(\\x: Bool. pred true) 0"
+    }
+
+    test("term1 term2") {
+        "(\\x: Nat. (\\dummy: Nat. \\y: Nat. y) 1) 0 ((\\x: Nat->Nat. x) 0)" shouldReduceTo "(\\dummy: Nat. \\y: Nat. y) 1 ((\\x: Nat->Nat. x) 0)"
+    }
+
 }
