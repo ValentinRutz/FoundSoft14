32af2aab43ff063262a1d21a5b7ba463aeda0de9
Valerian Pittet <valerian.pittet@epfl.ch>
2014-10-28 19:50:18 +0100

    unit test continued

--- 3-typed/src/test/scala/TestTermParser.scala
+++ 3-typed/src/test/scala/TestTermParser.scala
@@ -45,4 +45,25 @@ class TestTermParser extends FunSuite with Matchers with LambdaTest {
     test("Assignment example") {
         "\\x: Nat.\\y: Nat. x y x" shouldParseTo "\\x: Nat. (\\y: Nat. ((x y) x))"
     }
+
+    test("Parse 0") {
+        "0" shouldParseTo Zero
+    }
+
+  test("Parse succ") {
+    "succ 0" shouldParseTo Succ(Zero)
+  }
+  test("Parse pred") {
+    "pred 0" shouldParseTo Pred(Zero)
+  }
+    test("Parse Int") {
+        "1" shouldParseTo Succ(Zero)
+        "2" shouldParseTo "succ 1"
+        "3" shouldParseTo "succ 2"
+    }
+
+  test("Parse iszero") {
+    "iszero 0" shouldParseTo IsZero(Zero)
+  }
+
 }
---
+++ 3-typed/src/test/scala/TestTypeParser.scala
@@ -0,0 +1,25 @@
+import fos.SimplyTyped._
+import fos._
+import org.scalatest.{ Matchers, FunSuite }
+
+class TestTypeParser extends FunSuite with Matchers with LambdaTest {
+    test("Bool") { "Bool" shouldParseToType Bool }
+    test("Nat") { "Nat" shouldParseToType Nat }
+    test("Nat -> Bool") { "Nat -> Bool" shouldParseToType TypeFun(Nat, Bool) }
+    test("Nat * Bool") { "Nat * Bool" shouldParseToType TypePair(Nat, Bool) }
+    test("(Nat)") { "(Nat)" shouldParseToType "Nat" }
+    test("parenthesis") {
+        "(Nat -> Nat) -> Nat" shouldParseToType TypeFun(TypeFun(Nat, Nat), Nat)
+        "Nat -> (Nat -> Nat)" shouldParseToType TypeFun(Nat, TypeFun(Nat, Nat))
+    }
+    test("-> right associative") {
+        "Nat -> Nat -> Nat" shouldParseToType "Nat -> (Nat -> Nat)"
+    }
+    test("* right associative") {
+        "Nat * Nat * Nat" shouldParseToType "Nat * (Nat * Nat)"
+    }
+    test("* precedence over ->") {
+        "Nat -> Nat * Nat" shouldParseToType "Nat -> (Nat * Nat)"
+        "Nat * Nat -> Nat" shouldParseToType "(Nat * Nat) -> Nat"
+    }
+}
