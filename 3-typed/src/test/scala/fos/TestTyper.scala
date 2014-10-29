a9ae464dbd67b16b44adcffbd692c2c6013309ba
Damien Engels <engelsdamien@gmail.com>
2014-10-29 04:14:40 +0100

    Adds more typer tests

--- 3-typed/src/test/scala/fos/TestTyper.scala
+++ 3-typed/src/test/scala/fos/TestTyper.scala
@@ -8,13 +8,25 @@ class TestTyper extends FunSuite with Matchers with LambdaTest {
         an[TypeError] should be thrownBy typeof(parse(Term)("iszero true"))
     }
 
+    def expectedType(term: String, typ: String) =
+        test(s"$term should have type $typ") {
+            typeof(parse(Term)(term)) should be(parse(Type)(typ))
+        }
+
     val boolTyped = Seq("true", "false", "iszero 0",
         "if true then true else true", "let x : Bool = true in x",
         "fst {true, 0}", "snd {0, false}")
 
-    boolTyped foreach { term =>
-        test(s"$term should have type Bool") {
-            typeof(parse(Term)(term)) should be(TypeBool)
-        }
-    }
+    boolTyped.foreach(expectedType(_, "Bool"))
+
+    val intTyped = Seq("0", "succ 5", "pred 17",
+        "if true then 9 else 1", "let x : Nat = 7 in x",
+        "snd {true, 0}", "fst {0, false}")
+
+    intTyped.foreach(expectedType(_, "Nat"))
+
+    expectedType("{0, false}", "Nat*Bool")
+
+    expectedType("""\x : Nat.false""", "Nat->Bool")
+
 }
