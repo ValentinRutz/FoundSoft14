241fbcb609f39f61c7815bdebe7b94cc9f40c85a
Damien Engels <engelsdamien@gmail.com>
2014-10-30 09:38:44 +0100

    Adds checks that toString behaves correctly and can the term can be reparsed exactly

---
+++ 3-typed/src/test/scala/fos/ToStringChecks.scala
@@ -0,0 +1,37 @@
+import fos.SimplyTyped._
+import fos._
+import org.scalatest.{ Matchers, FunSuite }
+
+class ToStringChecks extends FunSuite with Matchers {
+
+    def parse[T](parser: Parser[T])(in: String): T = {
+        val tokens = new lexical.Scanner(in)
+        phrase(parser)(tokens) match {
+            case Success(res, _) => res
+            case Failure(msg, _) => fail(msg)
+            case _ => fail("Error")
+        }
+    }
+
+    val terms = Seq("true", "false", "if true then true else true", "0",
+        "pred 0", "succ 0", "iszero 0", "x", """\x:Nat.x""", "t t", "{0, 0}",
+        "fst {0, 0}", "snd {0, 0}", "{{0, true}, 0}", "(((((((0)))))))")
+
+    terms.foreach { term =>
+        test(s"toString should be well behaved on $term") {
+            val parsedTerm = parse(Term)(term)
+            parse(Term)(parsedTerm.toString) should equal(parsedTerm)
+        }
+    }
+
+    val types = Seq("Nat", "Bool", "Nat*Nat", "Nat->Bool",
+        "(Nat->Nat)->Bool", "(Nat*Nat)*Nat", "Nat*(Bool->Nat)",
+        "(Bool->Nat)*Bool", "(Bool->Bool)*(Nat->Nat)")
+
+    types.foreach { typ =>
+        test(s"toString should be well behaved on $typ") {
+            val parsedTyp = parse(Type)(typ)
+            parse(Type)(parsedTyp.toString) should equal(parsedTyp)
+        }
+    }
+}
