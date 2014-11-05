4dbb060721f11dbfe9c7d88d787bc594a1b0362e
Valerian Pittet <valerian.pittet@epfl.ch>
2014-11-05 20:55:05 +0100

    extended TestTypeParser to cover new Sum type

--- 3-typed/src/test/scala/TestTypeParser.scala
+++ 3-typed/src/test/scala/TestTypeParser.scala
@@ -7,6 +7,7 @@ class TestTypeParser extends FunSuite with Matchers with LambdaTest {
     test("Nat") { "Nat" shouldParseToType Nat }
     test("Nat -> Bool") { "Nat -> Bool" shouldParseToType TypeFun(Nat, Bool) }
     test("Nat * Bool") { "Nat * Bool" shouldParseToType TypePair(Nat, Bool) }
+    test("Nat + Bool") { "Nat + Bool" shouldParseToType TypeSum(Nat, Bool) }
     test("(Nat)") { "(Nat)" shouldParseToType "Nat" }
     test("parenthesis") {
         "(Nat -> Nat) -> Nat" shouldParseToType TypeFun(TypeFun(Nat, Nat), Nat)
@@ -18,8 +19,22 @@ class TestTypeParser extends FunSuite with Matchers with LambdaTest {
     test("* right associative") {
         "Nat * Nat * Nat" shouldParseToType "Nat * (Nat * Nat)"
     }
+    test("+ righ associative") {
+        "Nat + Nat + Nat" shouldParseToType "Nat + (Nat + Nat)"
+    }
     test("* precedence over ->") {
         "Nat -> Nat * Nat" shouldParseToType "Nat -> (Nat * Nat)"
         "Nat * Nat -> Nat" shouldParseToType "(Nat * Nat) -> Nat"
+    }
+
+    test("+ precedence over ->") {
+        "Nat -> Nat + Nat" shouldParseToType "Nat -> (Nat + Nat)"
+        "Nat + Nat -> Nat" shouldParseToType "(Nat + Nat) -> Nat"
+    }
+
+    test("+ same priority as *") {
+        "Nat * Nat + Nat" shouldParseToType "Nat * (Nat + Nat)"
+        "Nat + Nat * Nat" shouldParseToType "Nat + (Nat * Nat)"
+
     }
 }
