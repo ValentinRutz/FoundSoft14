250c64f662cf0fa94e1acfe944760b6c172d62e2
Valerian Pittet <valerian.pittet@epfl.ch>
2014-11-05 23:42:07 +0100

    added test for subst

---
+++ 3-typed/src/test/scala/fos/TestHelpers.scala
@@ -0,0 +1,45 @@
+import fos.SimplyTyped._
+import fos._
+import org.scalatest.{ Matchers, FunSuite }
+
+class TestSubst extends FunSuite with Matchers with LambdaTest {
+
+    val s = Variable("s")
+
+    test("var substitution") {
+        subst(x)("x", s) should be(s)
+    }
+
+    test("var non-substitution") {
+        subst(y)("x", s) should be(y)
+    }
+
+    test("no capturable vars, simple abstraction") {
+        // [y->s] \x.y
+        val tree = Abstraction(x, Nat, y)
+        val sTree = Abstraction(x, Nat, s)
+        subst(tree)("y", s) should be(sTree)
+    }
+
+    test("var shadowed in Abstraction not substituted") {
+        val term = "(\\x: Nat.(\\x: Nat. x)) 0"
+        val reduced = "\\x: Nat. x"
+        term shouldParseTo Application(Abstraction(x, Nat, Abstraction(x, Nat, x)), Zero())
+        term shouldReduceTo reduced
+    }
+
+    test("var shadowed in case inl not substituted") {
+        val term = "case inl 0 as Nat+Nat of inl x => \\x: Nat.(\\x: Nat. x) | inr y => y"
+        val reduced = "\\x: Nat.(\\x: Nat. x)"
+
+        term shouldReduceTo reduced
+    }
+
+    test("var shadowed in case inr not substituted") {
+        val term = "case inr 0 as Nat+Nat of inl y => y | inr  x => \\x: Nat.(\\x: Nat. x)"
+        val reduced = "\\x: Nat.(\\x: Nat. x)"
+
+        term shouldReduceTo reduced
+    }
+
+}
