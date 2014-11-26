420156861741de3b4a40ea8739fc3cfebd16c18f
Valerian Pittet <valerian.pittet@epfl.ch>
2014-11-26 14:10:56 +0100

    collect function

--- 5-inference/src/main/scala/fos/TwoPhaseInferencer.scala
+++ 5-inference/src/main/scala/fos/TwoPhaseInferencer.scala
@@ -1,4 +1,5 @@
 package fos
+import Type._
 
 /** Two-phase inferencer, first collect constraints, then solve them. */
 class TwoPhaseInferencer extends TypeInferencers {
@@ -14,12 +15,51 @@ class TwoPhaseInferencer extends TypeInferencers {
       *  constraint list.
       */
     def collect(env: Env, t: Term): TypingResult = t match {
-        case Var(x) =>
+        case True() | False() => TypingResult(TypeBool, noConstraints)
+        case Zero() => TypingResult(TypeNat, noConstraints)
+        case Pred(t) => {
+            val TypingResult(typ, constraints) = collect(env, t)
+            TypingResult(TypeNat, (TypeNat, typ) :: constraints)
+        }
+        case Succ(t) => {
+            val TypingResult(typ, constraints) = collect(env, t)
+            TypingResult(TypeNat, (TypeNat, typ) :: constraints)
+        }
+        case IsZero(t) => {
+            val TypingResult(typ, constraints) = collect(env, t)
+            TypingResult(TypeBool, (TypeNat, typ) :: constraints)
+        }
+        case If(c, t, e) => {
+            val TypingResult(cTyp, cCons) = collect(env, c)
+            val TypingResult(tTyp, tCons) = collect(env, t)
+            val TypingResult(eTyp, eCons) = collect(env, e)
+            TypingResult(tTyp,
+                (cTyp, TypeBool) :: (tTyp, eTyp) :: cCons ::: tCons ::: eCons)
+        }
+        case Var(x) => {
             val t1 = lookup(env, x)
             if (t1 == null)
                 throw TypeError("Unknown variable " + x)
             TypingResult(t1.instantiate, noConstraints)
-        //   ... To complete ...
+        }
+        case Abs(v, tp, t) => {
+            val tArg = tp match {
+                case EmptyType => freshTypeVar
+                case some => toType(some)
+            }
+
+            val tScheme = generalize(env, tArg)
+            val TypingResult(tRet, constraints) = collect((v, tScheme) :: env, t)
+            // Use tArg or tScheme.instantiate ?
+            TypingResult(TypeFun(tArg, tRet), constraints)
+        }
+        case App(t1, t2) => {
+            val TypingResult(t1Type, t1Cons) = collect(env, t1)
+            val TypingResult(t2Type, t2Cons) = collect(env, t2)
+            val resultType = freshTypeVar
+            TypingResult(resultType,
+                (t1Type, TypeFun(t2Type, resultType)) :: t1Cons ::: t2Cons)
+        }
     }
 
     /**
