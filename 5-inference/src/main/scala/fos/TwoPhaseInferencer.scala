1708755620c000eacaf018d80cfec97f81493cb8
Valerian Pittet <valerian.pittet@epfl.ch>
2014-11-27 01:20:28 +0100

    comment suggesting changing case abstraction in collect

--- 5-inference/src/main/scala/fos/TwoPhaseInferencer.scala
+++ 5-inference/src/main/scala/fos/TwoPhaseInferencer.scala
@@ -48,6 +48,8 @@ class TwoPhaseInferencer extends TypeInferencers {
                 case some => toType(some)
             }
 
+            // maybe not generalize but 
+            //val tScheme = TypeScheme(Nil, tArg)
             val tScheme = generalize(env, tArg)
             val TypingResult(tRet, constraints) = collect((v, tScheme) :: env, t)
             // Use tArg or tScheme.instantiate ?
