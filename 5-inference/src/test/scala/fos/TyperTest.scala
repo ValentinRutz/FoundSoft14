f4826f6843c395b642cf51cd19598d621757c9ef
Damien Engels <engelsdamien@gmail.com>
2014-12-03 20:57:11 +0100

    Added test cases

--- 5-inference/src/test/scala/fos/TyperTest.scala
+++ 5-inference/src/test/scala/fos/TyperTest.scala
@@ -7,7 +7,10 @@ class Typer extends TestSuite {
         """\x. let y = succ x in y""" -> "Nat->Nat",
         """\x. let x = x in succ x""" -> "Nat->Nat",
         """\x. x (let y = x in y 0)""" -> "(Nat->Nat)->Nat",
-        """\x. (\y. succ y) x""" -> "Nat->Nat")
+        """\x. (\y. succ y) x""" -> "Nat->Nat",
+        """(\z.(\y.\x.if x then x else x)) true succ 0""" -> "Bool->Bool",
+        """\x.\y. ((\a. (\b. succ b)) if true then x else y) y""" -> "Nat->Nat->Nat",
+        """\b. (let x = succ b in (\y. 0)) true""" -> "Nat->Nat")
 
     for ((term, tpe) <- terms) {
         testTypeOf(term, tpe)
