0f262f1da17c1b1676caf58c0d2b408cacc03536
Valerian Pittet <valerian.pittet@epfl.ch>
2014-12-20 21:58:41 +0100

    added helper and clearing warnings

--- 6-fj/src/main/scala/fos/Type.scala
+++ 6-fj/src/main/scala/fos/Type.scala
@@ -1,76 +1,89 @@
 package fos
 
-import scala.collection.mutable.{Map,HashMap};
-
-
+import scala.collection.mutable.{ Map, HashMap };
 
 case class TypeError(msg: String) extends Exception(msg)
 
 object Type {
 
-  import CT._
-  import Utils._
+    import CT._
+    import Utils._
 
-  type Class = String
-  type Context = List[Pair[Class, String]]
+    type Class = String
+    type Context = List[Pair[Class, String]]
 
-  def typeOf(tree: Tree, ctx: Context): Class =
-  //   ... To complete ... 
+    def typeOf(tree: Tree, ctx: Context): Class = ???
+    //   ... To complete ... 
 }
 
 case class EvaluationException(msg: String) extends Exception
 
 object Evaluate extends (Expr => Expr) {
 
-  import Utils._
+    import Utils._
 
-  def apply(expr: Expr): Expr =
-  //   ... To complete ... 
+    def apply(expr: Expr): Expr = ???
+    //   ... To complete ... 
 
-  def substituteInBody(exp: Expr, thiss: New, substs: List[(FieldDef,Expr)]): Expr = exp match {
-    case Select(obj: Expr, field: String)  => Select(substituteInBody(obj, thiss, substs),field)
-    case New(cls, args)                    => New(cls,args map (arg => substituteInBody(arg, thiss, substs)))
-    case Cast(cls, e)                      => Cast(cls,substituteInBody(e, thiss, substs))
-    case Var("this")                       => thiss
-    case Var(bd) => substs find (subs => subs._1.name == bd) match {
-        case None => exp
-        case Some((_,sub)) => sub
-      }
+    def substituteInBody(exp: Expr, thiss: New, substs: List[(FieldDef, Expr)]): Expr = exp match {
+        case Select(obj: Expr, field: String) => Select(substituteInBody(obj, thiss, substs), field)
+        case New(cls, args) => New(cls, args map (arg => substituteInBody(arg, thiss, substs)))
+        case Cast(cls, e) => Cast(cls, substituteInBody(e, thiss, substs))
+        case Var("this") => thiss
+        case Var(bd) => substs find (subs => subs._1.name == bd) match {
+            case None => exp
+            case Some((_, sub)) => sub
+        }
 
-    case Apply(obj,method,args)            => Apply(substituteInBody(obj, thiss, substs), method, args map (arg => substituteInBody(arg, thiss, substs)))
-    case _                                 => throw new EvaluationException("Apply: Forgot expression "+exp)
-  }
+        case Apply(obj, method, args) => Apply(substituteInBody(obj, thiss, substs), method, args map (arg => substituteInBody(arg, thiss, substs)))
+        case _ => throw new EvaluationException("Apply: Forgot expression " + exp)
+    }
 }
 
 object CT {
 
-  val objectClass: String = "Object"
-  private val objectClassDef = ClassDef(objectClass, null, Nil, CtrDef(objectClass, Nil, Nil, Nil) , Nil)
+    val objectClass: String = "Object"
+    private val objectClassDef = ClassDef(objectClass, null, Nil, CtrDef(objectClass, Nil, Nil, Nil), Nil)
 
-  private var ct: Map[String, ClassDef] = new HashMap[String, ClassDef]
+    private var ct: Map[String, ClassDef] = new HashMap[String, ClassDef]
 
-  add(objectClass,objectClassDef)
+    add(objectClass, objectClassDef)
 
-  def elements = ct iterator
+    def elements = ct iterator
 
-  def lookup(classname: String): Option[ClassDef] = if(classname != null) ct get classname else None
+    def lookup(classname: String): Option[ClassDef] = if (classname != null) ct get classname else None
 
-  def add(key: String, element: ClassDef): Unit = ct += key -> element
+    def add(key: String, element: ClassDef): Unit = ct += key -> element
 
-  def delete(key: String) = ct -= key
+    def delete(key: String) = ct -= key
 
-  def clear(): Unit = {
-    ct clear;
-    add(objectClass,objectClassDef)
-  }
+    def clear(): Unit = {
+        ct clear;
+        add(objectClass, objectClassDef)
+    }
+
+    /*-------------------------------------------------------*/
+    // Code added by Valerian
+
+    def firstInheritanceLoop: Option[ClassDef] = ct.values find { classDef =>
+        classDef isSuperclassOf classDef.superClass
+    }
+
+    def checkInheritanceLoop: Unit = firstInheritanceLoop match {
+        case None => ()
+        case Some(classDef) =>
+            throw new ClassHierarchyException(classDef.name + "is part of an inheritance loop")
+    }
+
+    // end of code added by Valerian
+    /*--------------------------------------------------------*/
 
 }
 
-
 object Utils {
 
-  def getClassDef(className: String): ClassDef = CT lookup className match {
-    case None => throw new TypeError("class "+className+" not declared")
-    case Some(c: ClassDef) => c
-  }
+    def getClassDef(className: String): ClassDef = CT lookup className match {
+        case None => throw new TypeError("class " + className + " not declared")
+        case Some(c: ClassDef) => c
+    }
 }
